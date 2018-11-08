435811ba46bb0aff55ad81fc8c430d16a9e75f46
HangKeon <whgkdrjs4321@naver.com>
2018-11-08 23:32:20 +0900

    My practice project name is MsgShareApp

---
+++ app/src/main/java/com/example/hangkeon/msgshareapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.hangkeon.msgshareapp
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        BtnShowToast.setOnClickListener()
+        {
+            //Code
+            Log.i("MainActivity", "Button was clicked !")
+
+            Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
