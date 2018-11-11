609c9a96c671f44e12166011cef76ed98b9e34b2
HangKeon <whgkdrjs4321@naver.com>
2018-11-11 20:43:46 +0900

    Add activity_second.xml & SeconActivity.kt & some codes

--- app/src/main/java/com/example/hangkeon/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/hangkeon/msgshareapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hangkeon.msgshareapp
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -17,7 +18,16 @@ class MainActivity : AppCompatActivity() {
             //Code
             Log.i("MainActivity", "Button was clicked !")
 
-            Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+            //Toast.makeText(this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener()
+        {
+            val message : String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
